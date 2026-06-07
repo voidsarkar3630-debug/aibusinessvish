49c497b8d4987b64c368e20e5d14b19b4ce8cd86
voidsarkar3630-debug <voidsarkar3630@gmail.com>
2026-06-07 18:37:55 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,3 +16,52 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getAuth,
+onAuthStateChanged,
+signOut
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_AUTH_DOMAIN",
+projectId: "YOUR_PROJECT_ID",
+storageBucket: "YOUR_BUCKET",
+messagingSenderId: "YOUR_MSG_ID",
+appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+document.getElementById("email").innerText=user.email;
+document.getElementById("uid").innerText=user.uid;
+
+document.getElementById("name").innerText=
+user.displayName || "AI BUSINESS User";
+
+}else{
+
+window.location="login.html";
+
+}
+
+});
+
+document.getElementById("logoutBtn").onclick=()=>{
+
+signOut(auth)
+.then(()=>{
+
+window.location="login.html";
+
+});
+
+};
