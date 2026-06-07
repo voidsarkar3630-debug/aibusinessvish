8b714499d2491fc4548d72daa0f2b04cb8b561e3
voidsarkar3630-debug <voidsarkar3630@gmail.com>
2026-06-07 18:23:42 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,45 @@
-console.log('login');
+import { auth } from "./firebase-config.js";
+
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const loginBtn =
+document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+try{
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+if(
+email ===
+"aibusinessvishwakarma@gmail.com"
+){
+window.location.href =
+"admin.html";
+}
+else{
+window.location.href =
+"dashboard.html";
+}
+
+}
+catch(error){
+
+alert(error.message);
+
+}
+
+});
